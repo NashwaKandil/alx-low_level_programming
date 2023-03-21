5f538b8dd425f81d568ded4ec83fcc92428092f1
NashwaKandil <nashwakandel@gmail.com>
2023-03-20 23:09:57 -0700

    task12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - start
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	int x;
+
+	i = 0;
+	x = 1;
+	while (i < 99)
+	{
+		while (x < 100)
+		{
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			putchar(' ');
+			putchar((i / 10) + '0');
+			putchar((x % 10) + '0');
+			if (i + x != 197)
+			{
+				putchar(',');
+				putchar(' ');
+			}	
+			x++;
+		}
+		i++;
+		x = i + 1;
+	}
+	putchar('\n');
+	return (0);
+}
