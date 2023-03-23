ae743aa4d8b3ea821583d5c13e5cb2f43af7e40b
NashwaKandil <nashwakandel@gmail.com>
2023-03-23 11:51:47 -0700

    task9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * times_table - start
+ *
+ * Return: 0
+ */
+
+void times_table(void)
+{
+	int i;
+	int v;
+	int x;
+
+	i = 0;
+	v = 0;
+	while (i < 10)
+	{
+		while (v < 10)
+		{
+			x = i * v;
+			if (x < 10)
+			{
+				_putchar(x + '0');
+				if (v != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			else
+			{
+				_putchar((x / 10) + '0');
+				_putchar((x % 10) + '0');
+				if (v != 9)
+				{
+					_putchar(',');
+				}
+			}
+			v++;
+		}
+		_putchar('\n');
+		v = 0;
+		i++;
+	}
+}
