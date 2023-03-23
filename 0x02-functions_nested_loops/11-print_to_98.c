192b9a10cd0ac61454f43736368a029d7d683209
NashwaKandil <nashwakandel@gmail.com>
2023-03-23 14:24:54 -0700

    task11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,17 +15,25 @@ void print_to_98(int n)
 	{
 		while (n <= 98)
 		{
-			printf("%d, ", n);
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
 			n++;
 		}
 	}
 	else if (n == 98)
-		printf("%d, ", n);
+		printf("%d", n);
 	else
 	{
-		while (n > 98)
+		while (n >= 98)
 		{
-			printf("%d, ", n);
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
 			n--;
 		}
 	}
