05f4145a5ede085f49c711b3b92c96b3840fc61d
NashwaKandil <nashwakandel@gmail.com>
2023-03-23 19:01:00 -0700

    task6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_line - straight line
+ * @n: length of line
+ * Return: 0
+ */
+
+void print_line(int n)
+{
+	if (n == 0 || n < 0)
+		putchar('\n');
+	else
+	{
+		while (n > 0)
+		{
+			putchar('_');
+			n--;
+		}
+	}
+	putchar('\n');
+}
