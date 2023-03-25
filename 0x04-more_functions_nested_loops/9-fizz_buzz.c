f95bbf624317de920be3c341d4e6df424f8e328f
NashwaKandil <nashwakandel@gmail.com>
2023-03-24 17:32:56 -0700

    task9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - start
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 1; x <= 100; x++)
+	{
+		if (x % 3 == 0 && x % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (x % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (x % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			printf("%d ", x);
+		}
+	}
+	printf("\n");
+	return (0);
+}
