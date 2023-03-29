e84b4b84fe0b35de030d6ec37bf9dcb66bad40e3
NashwaKandil <nashwakandel@gmail.com>
2023-03-28 17:12:28 -0700

    task1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps value of two ints
+ * @a: int pointed to by *a
+ * @*a: pointer
+ * @b: int pointed to by *b
+ * @*b: pointer
+ * Return: 0
+ */
+
+void swap_int(int *a, int *b)
+{
+
+	*a = 42;
+	*b = 98;
+}
