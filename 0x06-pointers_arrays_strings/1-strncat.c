109f5336d1f021b607df55c21731dd348344d5c6
NashwaKandil <nashwakandel@gmail.com>
2023-03-30 17:45:29 -0700

    task1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings to an n of bytes
+ * @dest: 1st string
+ * @src: 2nd string
+ * @n: limit
+ * Return: 0
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int destlen, i;
+
+	destlen = 0;
+	for (i = 0 ; dest[i] != '\0' ; i++)
+		destlen++;
+	for (i = 0 ; i < n ; i++)
+		dest[destlen + i] = src[i];
+	return (dest);
+}
