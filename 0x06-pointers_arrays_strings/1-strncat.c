550dd94b2f4786d48fd8c3d2f910a5bcba7265ec
NashwaKandil <nashwakandel@gmail.com>
2023-03-30 19:45:57 -0700

    task1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -15,7 +15,7 @@ char *_strncat(char *dest, char *src, int n)
 	destlen = 0;
 	for (i = 0 ; dest[i] != '\0' ; i++)
 		destlen++;
-	for (i = 0 ; i < n ; i++)
+	for (i = 0 ; i < n && src[i] != '\0' ; i++)
 		dest[destlen + i] = src[i];
 	return (dest);
 }
