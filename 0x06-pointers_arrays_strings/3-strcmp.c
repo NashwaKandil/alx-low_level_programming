4275855d35c9a3c038449369bcd2240c81669ee1
NashwaKandil <nashwakandel@gmail.com>
2023-03-30 19:02:17 -0700

    task3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: 1st string
+ * @s2: 2nd string
+ * Return: 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int f;
+
+	f = s1[0] - s2[0];
+	return (f);
+}
