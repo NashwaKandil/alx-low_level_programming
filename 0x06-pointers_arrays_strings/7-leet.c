3aeac6b73729c4c9aa0dc32b9a68bedaac1d8d9e
NashwaKandil <nashwakandel@gmail.com>
2023-03-31 15:09:47 -0700

    task7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,22 +8,19 @@
 
 char *leet(char *n)
 {
-	int i;
+	int i, x;
 
-	i = 0;
-	while (n[i] != '\0')
+	char a[] = "AaEeOoTtLl";
+	char b[] = "4433007711";
+	for (i = 0 ; n[i] != '\0' ; i++)
 	{
-		if (n[i] == 'a' || n[i] == 'A')
-			n[i] = '4';
-		else if (n[i] == 'e' || n[i] == 'E')
-			n[i] = '3';
-		else if (n[i] == 'o' || n[i] == 'O')
-			n[i] = '0';
-		else if (n[i] == 't' || n[i] == 'T')
-			n[i] = '7';
-		else if (n[i] == 'l' || n[i] == 'L')
-			n[i] = '1';
-		i++;
+		for (x = 0 ; x < 10 ; x++)
+		{
+			if (n[i] == a[x])
+			{
+				n[i] = b[x];
+			}
+		}
 	}
 	return (n);
 }
