7618fe43969c50ac3501d866f76b1c9e8b9a8674
NashwaKandil <nashwakandel@gmail.com>
2023-04-04 17:02:26 -0700

    task5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -12,13 +12,14 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
 
-	for (i = 0 ; needle[i] != '\0' ; i++)
+	for (i = 0 ; haystack[i] != '\0' ; i++)
 	{
-		for (j = 0 ; haystack[j] != '\0' ; j++)
+		for (j = 0 ; haystack[i] == needle[j] && needle[j] != '\0' ; j++)
 		{
-			if (needle[i] == haystack[j])
-				return (haystack + j);
+			i++;
 		}
+		if (needle[j] == '\0')
+			return (haystack + j);
 	}
 	return (NULL);
 }
