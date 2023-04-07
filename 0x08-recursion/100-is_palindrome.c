dd826458f1d2d81d5060484692af4b305c9928dd
NashwaKandil <nashwakandel@gmail.com>
2023-04-06 19:58:16 -0700

    task7

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * is_palindrome - returns 1 if a string is a palindrome and 0 if not
+ * @s: string to check
+ * Return: 1 or 0
+ */
+
+int is_palindrome(char *s)
+{
+	char *z;
+
+	if (*s != '\0')
+	{
+		is_palindrome(s + 1);
+		z = s;	
+	}
+	if (z == s)
+		return (1);
+	else
+		return (0);
+}
