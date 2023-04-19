6beeca4bda943b98b0bfaff0270dd91932171d1c
NashwaKandil <nashwakandel@gmail.com>
2023-04-19 14:00:24 -0700

    task2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -5,20 +5,14 @@
 /**
  * print_dog - prints a struct dog
  * @d: struct dog variable
- * Return: 0
  */
 
 void print_dog(struct dog *d)
 {
-	char *nil;
-
-	nil = NULL;
 	if (d->name == NULL)
 		d->name = "(nil)";
 	if (d->owner == NULL)
 		d->owner = "(nil)";
-	if (d->age == 0)
-		d->age = (*nil);
 	if (d == NULL)
 		return;
 	printf("Name: %s\n", d->name);
