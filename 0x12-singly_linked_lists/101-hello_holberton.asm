3b14cc847f7c8b87e9f9c9dfa98b288ecb62a9a9
NashwaKandil <nashwakandel@gmail.com>
2023-04-28 19:22:36 -0700

    task6

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -2,9 +2,11 @@ section		.string
 	extern	printf
 	global	main
 main:
-	mov	eax, 0
-	mov	edi, text
+	mov		edi, text
+	xor		eax, eax
 	call	printf
+	mov		eax, 0
+	ret
 
 section		.format
-	text db 'Hello, Holberton\n', 0
+	text: db 'Hello, Holberton', 0xa, 0
