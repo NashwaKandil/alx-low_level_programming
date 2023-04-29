7ecaddc3e98f5af2398ed31e42f3abe1fb16d671
NashwaKandil <nashwakandel@gmail.com>
2023-04-29 16:46:43 -0700

    task4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * num_check - checks for digit
+ * @s: string to check
+ * Return: 0
+ */
+
+int num_check(char *s)
+{
+	unsigned int i;
+
+	for (i = 0; i < strlen(s); i++)
+	{
+		if (!isdigit(s[i]))
+			return (0);
+	}
+	return (1);
+}
+
+/**
+ * main - adds positive numbers
+ * argc: arguments number
+ * argv: arguments array
+ * Return: sum or 1 when error
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, str, res;
+
+	res = 0;
+	for (i = 1; i < argc; i++)
+	{
+		if (num_check(argv[i]))
+		{
+			str = atoi(argv[i]);
+			res += str;
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", res);
+	return (0);
+}
