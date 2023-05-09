8b6812e41d37f2da504ec974a1f1096f295da80d
NashwaKandil <nashwakandel@gmail.com>
2023-05-08 19:25:09 -0700

    tas1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file.
+ * @filename: file's name
+ * @text_content: text in the file
+ * Return: 1 or -1
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fds, ln, w;
+
+	if (filename == NULL || text_content == NULL)
+		return (-1);
+	ln = 0;
+	while (text_content[ln] != '\0')
+		ln++;
+	fds = open(filename, O_CREAT | O_RDWR | O_TRUNC);
+	w = write(fds, text_content, ln);
+	if (w == -1 || fds == -1)
+		return (-1);
+	close(fds);
+	return (1);
+}
